60709d4f2127c5b19d5314fa619a663e15d67f05
VincBar <51025193+VincBar@users.noreply.github.com>
2020-11-07 23:04:23 +0100

    added callback for updating second tab

--- webpage_structure/second_tab_structure.py
+++ webpage_structure/second_tab_structure.py
@@ -1,7 +1,8 @@
 import dash_core_components as dcc
 import dash_html_components as html
 from datetime import date
-
+import dash_table
+import pandas as pd
 
 def main_structure():
     return html.Div([
@@ -15,23 +16,48 @@ def main_structure():
                 end_date=date(2021, 7, 11),
                 style={"margin-left": "80px", "margin-top": "15px", "margin-bottom": "4px", 'float': 'left'}
             ),
-            dcc.Dropdown(
+            html.Div(dcc.Dropdown(
                 id='ordering-selection',
                 options=[
                     {'label': 'Most severe', 'value': 'severe'},
                     {'label': 'Highest normal traffic', 'value': 'normal'},
                     {'label': 'Highest Capacity Reduction', 'value': "capacity"},
-                    {'label': 'Most Days with Reduction', 'value': 'time'}
+                    {'label': 'Most Days with Reduction', 'value': 'time'},
+                    {'label': 'Most Conflicts', 'value': 'conflict'},
                 ],
                 value='severe',
-                style={"margin-left": "20px", "margin-top": "19px", 'float': 'left'}
-            )], style={"margin-left": "10px", "margin-bottom": "10px", "display": "table-row"})
+                style={"margin-left": "20px", "margin-top": "19px","width":"400px"}
+            ),style={'float': 'right'})], style={"margin-left": "10px", "margin-bottom": "10px", "display": "table-row","width":"100%"})
         ,
-        html.Div([html.Div(style={"width": "31%", "height": "99%", "border": "1px solid black", "float": "left",
+        html.Div([html.Div(id="first_column",
+                           style={"width": "31%", "height": "99%", "border": "1px solid black", "float": "left",
                                   "margin-left": "10px"}),
-                  html.Div(style={"width": "31%", "height": "99%", "border": "1px solid black", "float": "left",
+                  html.Div(id="second_column",
+                           style={"width": "31%", "height": "99%", "border": "1px solid black", "float": "left",
                                   "margin-left": "10px"}),
-                  html.Div(style={"width": "31%", "height": "99%", "border": "1px solid black", "float": "left",
+                  html.Div(id="third_column",
+                           style={"width": "31%", "height": "99%", "border": "1px solid black", "float": "left",
                                   "margin-left": "10px"})],
                  style={"width": "99%", "height": "80vh", "border": "1px solid black"})
     ])
+
+import numpy as np
+
+
+def severe(column=0):
+    # some getting df stuff.
+    df = pd.DataFrame(np.ones((6,2)), index=["Usual Number of Trains",
+                                       "Usual Freight Capacity",
+                                       "Reduced Train Capacity",
+                                       "Reduced Freight Capacity",
+                                       "Difference in Trains",
+                                       "Difference in Freight"])
+    df.reset_index(inplace=True)
+
+    return html.Div([
+        dash_table.DataTable(
+            id='table_focus_{}'.format(column),
+            columns=[{"name": i, "id": i} for i in df.columns],
+            data=df.to_dict('records'))
+    ])
+
